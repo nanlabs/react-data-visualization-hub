6053fac2ea879db9a109ec772cc9deb24f3be982
pamcontin <pamcontin@gmail.com>
2024-04-25 09:51:43 -0300

    Config and install highcharts

--- src/pages/Highcharts/Highcharts.tsx
+++ src/pages/Highcharts/Highcharts.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import * as Highchart from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
 
-const Highcharts = () => {
+const options: Highchart.Options = {
+  title: {
+    text: 'My chart',
+  },
+  series: [
+    {
+      type: 'line',
+      data: [1, 2, 3],
+    },
+  ],
+};
+
+const Highcharts = (props: HighchartsReact.Props) => {
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+
   return (
     <div>
-      <h1>Highcharts</h1>
+      <HighchartsReact highcharts={Highchart} options={options} ref={chartComponentRef} {...props} />
     </div>
   );
 };
